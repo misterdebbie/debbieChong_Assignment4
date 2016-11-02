af7bd57ae03fdd9bcb97f2127011321ed129178e
mr.debbie <giyoungchong1980@gmail.com>
2016-11-01 20:54:07 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger = require('morgan')
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app_server/routes/index');
+var users = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
