7d18332880bf61f7e5e6d1120e91f72d03d7ae65
mr.debbie <giyoungchong1980@gmail.com>
2016-11-01 20:50:18 -0500

    update jade/views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ctrlNotes = require('../controllers/notes.js');
-var ctrlOthers = require('../controllers/others.js');
+//var ctrlOthers = require('../controllers/others.js');
 //this will GET home page
 //direct the route to look for this in the url request
 //Location pages
 router.get('/', ctrlNotes.loginHome);
 router.get('/note', ctrlNotes.noteEdit);
 router.get('/title', ctrlNotes.titleEdit);
-router.get('/about', ctrlOthers.about);
+//router.get('/about', ctrlOthers.about);
 //reference index method of controllers in route definition
 //the second parameter of router.get method references the controller function
 module.exports = router;
